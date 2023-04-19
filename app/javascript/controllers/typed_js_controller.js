ef1003c3ec20746cc1f345b2f372e5dd4a9f484e
Anna Mahe <annamahe9@gmail.com>
2023-04-19 09:46:51 +0200

    fixed cursor bug on dark mode screens

--- app/javascript/controllers/typed_js_controller.js
+++ app/javascript/controllers/typed_js_controller.js
@@ -4,7 +4,7 @@ import Typed from "typed.js"
 // Connects to data-controller="typed-js"
 export default class extends Controller {
   connect() {
-    new Typed('.element', {
+    new Typed(this.element, {
       strings: ["Développeuse Fullstack Junior", "Conception Développement d'Applications Web"],
       typeSpeed: 30,
       backSpeed: 20,
